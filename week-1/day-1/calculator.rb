cf2a89ba442553a5d155b9ad6da84467e518b7df
saida brito <saidabrito57@gmail.com>
2020-04-13 12:26:22 -0500

    Added spaces between letters and operatiors

--- week-1/day-1/calculator.rb
+++ week-1/day-1/calculator.rb
@@ -1,27 +1,29 @@
 class Calculator
-    def add(a,b)
-        a + b
-    end 
+  def add(a,b)
+    a + b
+  end 
 
-    def subtract(a,b)
-        a - b
-    end
+  def subtract(a,b)
+    a - b
+  end
 
-    def divide(a,b)
-        a/b
-    end
+  def divide(a,b)
+    a / b
+  end
             
-    def multiply(a,b)
-        a*b
-    end
+  def multiply(a,b)
+    a * b
+  end
     
-    def exponentiated(a,b)
-        a**b
+  def exponentiated(a,b)
+    a ** b
             
-    end
+  end
 
-    def modulo(a,b)
-        a%b
-    end
+  def modulo(a,b)
+    a % b
+  end
 end
+
+ 
 
