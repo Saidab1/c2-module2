b924e413937918f836aa530875f66889c2cc0c00
saida brito <saidabrito57@gmail.com>
2020-04-13 21:54:58 -0500

    Chnged puts and bmi formula

--- week-1/day-1/bmi-generator.rb
+++ week-1/day-1/bmi-generator.rb
@@ -4,6 +4,6 @@ user_weight = gets.chomp.to_f
 print ("Show me the height(m):")
 user_height = gets.chomp.to_f
 
-bmi = (user_weight / (user_height ** 2)).round(2)
+bmi = (user_weight / user_height ** 2).round(2)
 
-puts ("#{bmi}")
+puts bmi
