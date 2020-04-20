f5c74b11de7478ac87034d103a28c47fd047ac64
saida brito <saidabrito57@gmail.com>
2020-04-20 14:39:15 -0500

    added parenthesis to raise msg

--- week-1/day-4/put-n.rb
+++ week-1/day-4/put-n.rb
@@ -1,8 +1,8 @@
 def put_n(string,n)
   if !n.is_a? Integer
-    raise ArgumentError.new, "Only integers are allowed"
+    raise ArgumentError.new("Only integers are allowed")
   elsif n < 0 
-    raise ArgumentError.new, "n has to be positive"
+    raise ArgumentError.new("n has to be positive")
   end 
   
   n.times{puts string} 
